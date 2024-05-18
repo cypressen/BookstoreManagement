2f482c5b963d269ccdde4d624c61d1d651fe54d4
サイプレス <142464068+cypressen@users.noreply.github.com>
2024-05-18 18:29:48 +0800

    add some comments in ContainerClass

--- logic/ContainerClass/Book.h
+++ logic/ContainerClass/Book.h
@@ -13,17 +13,17 @@ private:
 public:
     Book(const std::string &title, const std::string &press,double price = 0.0);
     Book() = default;
-    std::string getID() const;
+    std::string getID() const; // return title + press
     void setPrice(double price);
     double getPrice() const; 
     std::string getTitle() const;
-    bool operator==(const Book &other) const;
-    std::string toFileStr()const;
-    void initFromFileStr(const std::vector<std::string>& line);
+    bool operator==(const Book &other) const; // 辅助用于BookHasher
+    std::string toFileStr()const; // 转换为用于写入文件的字符串
+    void initFromFileStr(const std::vector<std::string>& line); // 从对应的字符串初始化该对象
     // bool operator<(const Book &other) const;
 };
 
-class BookHasher
+class BookHasher // 为了使用unordered_map储存Book类型
 {
 public:
     size_t operator()(const Book &b) const;
